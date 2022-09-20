5cef3b9458def63e3c5b959625b791b680edb430
Anisa Elok Asmaransyah <106418871+AnisaElokAsmaransyah@users.noreply.github.com>
2022-09-20 17:52:07 +0700

    Update tugas5.js

--- tugas5.js
+++ tugas5.js
@@ -1,59 +1,17 @@
-    function hitung(){
+    function hitung(operasi){
         //tangkap id form
         var frm = document.getElementById('calcForm');
         var a1 = parseFloat(frm.angka1.value);
         var a2 = parseFloat(frm.angka2.value);
-        var tombol = frm.tombol.value;
-        switch (tombol) {
-            case 'btn_tambah':
-                if (isNaN(a1) || isNaN(a2)) {
-                    alert("Harap masukkan angka!!");
-                }    
-                else {
-                    var total = a1 + a2; //rumus
-                    frm.hasil.value = total; //penempatan hasil 
-                } 
-                break;
-            case 'btn_kurang':
-                if (isNaN(a1) || isNaN(a2)) {
-                    alert("Harap masukkan angka!!");
-                }    
-                else {
-                    var total = a1 - a2; //rumus
-                    frm.hasil.value = total; //penempatan hasil 
-                } 
-                break;
-            case 'btn_kali':
-                    if (isNaN(a1) || isNaN(a2)) {
-                        alert("Harap masukkan angka!!");
-                    }    
-                    else {
-                        var total = a1 * a2; //rumus
-                        frm.hasil.value = total; //penempatan hasil 
-                    } 
-                    break;
-            case 'btn_bagi':
-                        if (isNaN(a1) || isNaN(a2)) {
-                            alert("Harap masukkan angka!!");
-                        }    
-                        else {
-                            var total = a1 / a2; //rumus
-                            frm.hasil.value = total; //penempatan hasil 
-                        } 
-                        break;
-            case 'btn_pangkat':
-                            if (isNaN(a1) || isNaN(a2)) {
-                                alert("Harap masukkan angka!!");
-                            }    
-                            else {
-                                var total = Math.pow(a1,a2); //rumus
-                                frm.hasil.value = total; //penempatan hasil 
-                            } 
-                            break;
-                default:
-                    break;
+        if (isNaN(a1) || isNaN(a2)) return alert('Harap masukkan angka');
+
+        switch (operasi) {
+            case '+': total = a1 + a2; break;
+            case '-': total = a1 - a2; break;
+            case '*': total = a1 * a2; break;
+            case '/': total = a1 / a2; break;
+            case '^': total = Math.pow (a1,a2); break;
+            default : total = 0;
         }
-        
+        frm.hasil.value = total;
     }
-
-
